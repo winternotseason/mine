689cf2b79163fbbead772322d05ed4266b312d0d
winternotseason <winternotseason@naver.com>
2024-06-27 00:17:58 +0900

    나눔고딕 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/header";
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
+import { Nanum_Gothic } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nanumGothic = Nanum_Gothic({
+  subsets: ["latin"],
+  weight: ["400","700","800"]
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={nanumGothic.className}>
         <Header />
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
 import classes from "./header.module.css";
 import { FaShoppingBasket } from "react-icons/fa";
+
+
 const Header = () => {
   return (
-    <header className={classes.header}>
-      <Image
-        src="/mine-logo.png"
-        alt="mine logo"
-        width={30}
-        height={30}
-        priority
-      />
-      <FaShoppingBasket size={30} color="#526DFE"/>
+    <header className={`${classes.header}`}>
+      <p>
+        <Image
+          src="/mine-logo.png"
+          alt="mine logo"
+          width={30}
+          height={30}
+          priority
+        />
+        <span>쇼핑</span>
+      </p>
+      <p>
+        <FaShoppingBasket size={30} color="#526DFE" />
+      </p>
     </header>
   );
 };
