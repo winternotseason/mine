a3d5df5b63710565042e6794f5b0016f97315952
winternotseason <winternotseason@naver.com>
2024-06-27 16:52:23 +0900

    서버 환경에 따라 달라지는 fetch URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            hostname: "shopping-phinf.pstatic.net",
-          },
-         
-        ],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "shopping-phinf.pstatic.net",
       },
+    ],
+  },
+  env: {
+    NAVER_CLIENT_ID: "BoJTxbnp8XfeqDvtDwHc",
+    NAVER_CLIENT_SECRET: "wUmPD8WYJB",
+    NEXTAUTH_URL: isProd
+      ? "https://mine-shopping.vercel.app/"
+      : "http://localhost:3000/",
+  },
 };
 
 export default nextConfig;
