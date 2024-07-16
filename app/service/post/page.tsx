5c4fca2fea0124fdfb561b98e43ceabaebfceb5a
winternotseason <winternotseason@naver.com>
2024-07-16 17:30:50 +0900

    post page : use client

--- app/service/post/page.tsx
+++ app/service/post/page.tsx
@@ -1,11 +1,14 @@
+"use client";
+
 import React from "react";
 import classes from "./page.module.css";
 import { post } from "@/actions/post";
 import { auth } from "@/app/auth";
+import { useSession } from "next-auth/react";
 
-const PostPage = async () => {
-  const session = await auth();
-  
+const PostPage = () => {
+  const { data: session } = useSession();
+
   return (
     <div className={classes.container}>
       <h1>1:1 문의</h1>
