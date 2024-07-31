99f3b69a3d227dc5e40cf7aeee539dba2ed34208
winternotseason <winternotseason@naver.com>
2024-07-31 16:16:15 +0900

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  if (!request.cookies.get("authjs.session-token")) {
+  if (!request.cookies.get("authjs.session-token") || !request.cookies.get("__Secure-authjs.session-token")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 }
